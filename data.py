9200afa536c61926cd8c253855460170e40c2cfd
Ronald Canepa <77946353+LiquidRonnie@users.noreply.github.com>
2021-06-23 22:11:47 -0700

    Add files via upload
    submitted HTML homework

---
+++ data.py
@@ -0,0 +1,8 @@
+import pandas as pd
+# Read the csv file in
+df = pd.read_csv('Resources/cities.csv')
+# Save to file
+df.to_html('data.html', index=False)
+# Assign to string
+table = df.to_html()
+print(table)
